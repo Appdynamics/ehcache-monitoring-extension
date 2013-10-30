77ee6ecda7f3ed216e95f1f9f9548f576ee52b24
rvasanda <rohit.vasandani@osxltrvasa.local>
2013-10-30 15:15:43 -0700

    Completed monitor implementation
    Needs to be tested and cleaned up

--- src/main/java/EhcacheMonitor.java
+++ src/main/java/EhcacheMonitor.java
@@ -1,42 +1,84 @@
 import com.singularity.ee.agent.systemagent.api.AManagedMonitor;
+import com.singularity.ee.agent.systemagent.api.MetricWriter;
 import com.singularity.ee.agent.systemagent.api.TaskExecutionContext;
 import com.singularity.ee.agent.systemagent.api.TaskOutput;
 import com.singularity.ee.agent.systemagent.api.exception.TaskExecutionException;
-import net.sf.ehcache.Cache;
-import net.sf.ehcache.CacheManager;
-import net.sf.ehcache.Ehcache;
-import net.sf.ehcache.statistics.StatisticsGateway;
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
 
-import javax.management.MBeanServerConnection;
-import javax.management.ObjectInstance;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
 
 public class EhcacheMonitor extends AManagedMonitor{
 
-    public static void main(String[] args) {
-        try {
-            CacheManager cacheManager = CacheManager.newInstance();
-            CacheManager cacheManager2 = CacheManager.newInstance();
+    private static final String METRIC_PREFIX = "Custom Metrics|Ehcache|CacheId|";
+    private static final String HOST_PARAM = "host";
+    private static final String PORT_PARAM = "port";
+    private static final String DEFAULT_HOST_VALUE = "localhost";
+    private static final String DEFAULT_PORT_VALUE = "8080";
 
-            if (cacheManager.equals(cacheManager2)) {
-                System.out.println("what");
-            }
-            cacheManager.addCache("newCache");
-            Cache newCache = cacheManager.getCache("newCache");
-            StatisticsGateway statisticsGateway = newCache.getStatistics();
-            CacheManager.getInstance().shutdown();
-            System.out.println("Hehroh");
-            MBeanServerConnection x;
-        }
-        catch (Exception e) {
-            System.out.println("Doing something stupid");
-        }
+    private static final Logger logger = Logger.getLogger(EhcacheMonitor.class.getSimpleName());
 
-
+    public static void main(String[] args) throws Exception{
+        logger.setLevel(Level.DEBUG);
+        EhcacheMonitor ehcacheMonitor = new EhcacheMonitor();
+        Map<String,String> taskArguments = new HashMap<String, String>();
+        taskArguments.put(HOST_PARAM, DEFAULT_HOST_VALUE);
+        taskArguments.put(PORT_PARAM, DEFAULT_PORT_VALUE);
+        ehcacheMonitor.execute(taskArguments, null);
     }
 
     @Override
-    public TaskOutput execute(Map<String, String> stringStringMap, TaskExecutionContext taskExecutionContext) throws TaskExecutionException {
-        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    public TaskOutput execute(Map<String, String> taskArguments, TaskExecutionContext taskExecutionContext) throws TaskExecutionException {
+        logger.debug("Exceuting EhcacheMonitor...");
+        try {
+            EhcacheRESTWrapper ehcacheRESTWrapper = new EhcacheRESTWrapper(taskArguments.get("host"), taskArguments.get("port"));
+
+            HashMap metrics = ehcacheRESTWrapper.gatherMetrics();
+            logger.debug("Gathered metrics successfully");
+            printMetrics(metrics);
+            logger.debug("Printed metrics successfully");
+            return new TaskOutput("Task successful...");
+        } catch (Exception e) {
+          logger.error("Error: ", e);
+          return new TaskOutput("Task failed with error : " + e.getMessage());
+        }
+    }
+
+    private void printMetrics(HashMap metricsMap) throws Exception{
+        HashMap<String, HashMap<String, Number>> metrics = (HashMap<String,HashMap<String,Number>>) metricsMap;
+        Iterator outerIterator = metrics.keySet().iterator();
+
+        while (outerIterator.hasNext()) {
+            String id = outerIterator.next().toString();
+            HashMap<String, Number> cacheStatistics = metrics.get(id);
+            Iterator innerIterator = cacheStatistics.keySet().iterator();
+            while (innerIterator.hasNext()) {
+                String metricName = innerIterator.next().toString();
+                Number metric = cacheStatistics.get(metricName);
+                printMetric(id + "|" + metricName, metric,
+                        MetricWriter.METRIC_AGGREGATION_TYPE_OBSERVATION,
+                        MetricWriter.METRIC_TIME_ROLLUP_TYPE_AVERAGE,
+                        MetricWriter.METRIC_CLUSTER_ROLLUP_TYPE_INDIVIDUAL);
+            }
+        }
+    }
+    /**
+     * Returns the metric to the AppDynamics Controller.
+     * @param 	metricName		Name of the Metric
+     * @param 	metricValue		Value of the Metric
+     * @param 	aggregation		Average OR Observation OR Sum
+     * @param 	timeRollup		Average OR Current OR Sum
+     * @param 	cluster			Collective OR Individual
+     */
+    private void printMetric(String metricName, Number metricValue, String aggregation, String timeRollup, String cluster) throws Exception
+    {
+        MetricWriter metricWriter = super.getMetricWriter(METRIC_PREFIX + metricName,
+                aggregation,
+                timeRollup,
+                cluster
+        );
+        metricWriter.printMetric(String.valueOf((long) metricValue.doubleValue()));
     }
 }
---
+++ src/main/java/EhcacheRESTWrapper.java
@@ -0,0 +1,100 @@
+import org.apache.commons.lang.math.NumberUtils;
+import org.w3c.dom.Document;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.HashMap;
+
+public class EhcacheRESTWrapper {
+
+    private final static String CACHE_NODE = "cache";
+    private final static String CACHE_STATISTICS_NODE = "statistics";
+    private final static String CACHE_NODE_NAME = "name";
+
+    private String host;
+    private String port;
+
+    public EhcacheRESTWrapper(String host, String port) {
+        this.host = host;
+        this.port = port;
+    }
+
+    public HashMap gatherMetrics() throws Exception{
+        HttpURLConnection connection = null;
+        InputStream is = null;
+        HashMap metrics = new HashMap();
+        try {
+            String cacheServerUrl = constructURL();
+            URL u = new URL(cacheServerUrl);
+            connection = (HttpURLConnection) u.openConnection();
+            connection.setRequestMethod("GET");
+            connection.connect();
+            is = connection.getInputStream();
+            metrics = convertResponseToMap(is);
+            return metrics;
+        } catch(Exception e) {
+            throw e;
+        } finally {
+            try {
+                is.close();
+                connection.disconnect();
+            } catch (NullPointerException npe) {
+                // log error
+            } catch (Exception e) {
+                // log error
+            }
+        }
+    }
+
+    private HashMap convertResponseToMap(InputStream is) throws Exception{
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+        dbFactory.setNamespaceAware(true);
+        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+        Document doc = dBuilder.parse(is);
+
+        NodeList caches = doc.getElementsByTagName(CACHE_NODE);
+        HashMap<String, HashMap<String, Number>> metricsMap = new HashMap<String, HashMap<String, Number>>();
+        for (int i = 0; i < caches.getLength(); i++) {
+            Node cache = caches.item(i);
+            NodeList cacheNodes = cache.getChildNodes();
+            String cacheName = null;
+            for (int j = 0; j < cacheNodes.getLength(); j++) {
+                Node cacheNode = cacheNodes.item(j);
+                if (cacheNode.getNodeName().equalsIgnoreCase(CACHE_NODE_NAME)) {
+                    cacheName = cacheNode.getTextContent();
+                    metricsMap.put(cacheNode.getTextContent(), new HashMap<String,Number>());
+                }
+                if (cacheNode.getNodeName().equalsIgnoreCase(CACHE_STATISTICS_NODE)) {
+                    NodeList statistics = cacheNode.getChildNodes();
+                    HashMap<String, Number> statisticsMap = new HashMap<String, Number>();
+                    for (int k = 0; k < statistics.getLength(); k++) {
+                        Node statisticsNode = statistics.item(k);
+                        Number value;
+                        if (NumberUtils.isNumber(statisticsNode.getTextContent())) {
+                            if (statisticsNode.getTextContent().contains(".")) {
+                                value = Double.parseDouble(statisticsNode.getTextContent());
+                            }
+                            else {
+                                value = Long.parseLong(statisticsNode.getTextContent());
+                            }
+                            statisticsMap.put(statisticsNode.getNodeName(), value);
+                        }
+                    }
+                    if (cacheName != null) {
+                        metricsMap.put(cacheName, statisticsMap);
+                    }
+                }
+            }
+        }
+        return metricsMap;
+    }
+
+    private String constructURL() {
+        return new StringBuilder().append("http://").append(host).append(":").append(port).append("/ehcache/rest").toString();
+    }
+}
